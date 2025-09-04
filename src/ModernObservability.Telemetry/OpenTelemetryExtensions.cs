21f3d0ddb10ccf20408d140a5eddc84e43517e9b
Martin Thwaites <github@my2cents.co.uk>
2025-09-04 17:36:19 +0100

    Add telemetry project

---
+++ src/ModernObservability.Telemetry/OpenTelemetryExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+
+namespace ModernObservability.Telemetry;
+
+public static class OpenTelemetryExtensions
+{
+    public static IServiceCollection SetupOpenTelemetry(this IServiceCollection services)
+    {
+        services.AddOpenTelemetry()
+            .UseOtlpExporter()
+            .WithTracing(builder => builder
+                .AddAspNetCoreInstrumentation()
+                .AddSource("ModernObservability.*"));
+
+        return services;
+    }
+}
