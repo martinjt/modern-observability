c0f9dcc207cae302cecd50a9aadf7fb85007d75f
Martin Thwaites <github@my2cents.co.uk>
2025-09-06 17:56:49 +0100

    Reverted Emailsender to single message

--- src/ModernObservability.EmailSender/Program.cs
+++ src/ModernObservability.EmailSender/Program.cs
@@ -7,22 +7,22 @@ using ModernObservability.EmailSender;
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) =>
     {
-        // services.AddSingleton((sp) =>
-        // {
-        //     var serviceBusClient = new ServiceBusClient(sp.GetRequiredService<IConfiguration>().GetConnectionString("greetings"));
-        //     return serviceBusClient.CreateProcessor("greetings");
-        // });
-        //services.AddHostedService<MessageProcessorService>();
-        // Uncomment this to enable batch processing, comment out the one above when you do
         services.AddSingleton((sp) =>
         {
             var serviceBusClient = new ServiceBusClient(sp.GetRequiredService<IConfiguration>().GetConnectionString("greetings"));
-            return serviceBusClient.CreateReceiver("greetings", new ServiceBusReceiverOptions
-            {
-                ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete,
-            });
+            return serviceBusClient.CreateProcessor("greetings");
         });
-        services.AddHostedService<BatchMessageProcessorService>();
+        services.AddHostedService<MessageProcessorService>();
+        // Uncomment this to enable batch processing, comment out the one above when you do
+        // services.AddSingleton((sp) =>
+        // {
+        //     var serviceBusClient = new ServiceBusClient(sp.GetRequiredService<IConfiguration>().GetConnectionString("greetings"));
+        //     return serviceBusClient.CreateReceiver("greetings", new ServiceBusReceiverOptions
+        //     {
+        //         ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete,
+        //     });
+        // });
+        // services.AddHostedService<BatchMessageProcessorService>();
         services.AddSingleton<SMTPSender>();
     })
     .Build();
