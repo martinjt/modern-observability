7a413460cfb20a32daa6f5646da0444003b4913f
Martin Thwaites <github@my2cents.co.uk>
2025-09-06 15:48:07 +0100

    Added Meters

--- src/ModernObservability.Telemetry/OpenTelemetryExtensions.cs
+++ src/ModernObservability.Telemetry/OpenTelemetryExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using OpenTelemetry;
@@ -11,6 +12,10 @@ public static class DiagnosticSettings
 {
     private readonly static Lazy<ActivitySource> ActivitySourceInternal = new(() => new ActivitySource(Assembly.GetExecutingAssembly().GetName().Name!));
     public static ActivitySource ActivitySource = ActivitySourceInternal.Value;
+
+    public static Lazy<Meter> Meter = new(() => new Meter(Assembly.GetExecutingAssembly().GetName().Name!));
+    public static Histogram<double> GreetedAge = Meter.Value.CreateHistogram<double>("greeted_age", "years", "Age of greeted person");
+    public static Counter<int> EmailsSent = Meter.Value.CreateCounter<int>("emails_sent", "emails", "Number of emails sent");
 }
 
 public static class OpenTelemetryExtensions
@@ -25,7 +30,12 @@ public static class OpenTelemetryExtensions
             .WithTracing(builder => builder
                 .AddAspNetCoreInstrumentation()
                 .AddHttpClientInstrumentation()
-                .AddSource(DiagnosticSettings.ActivitySource.Name));
+                .AddSource(DiagnosticSettings.ActivitySource.Name))
+            .WithMetrics(builder => builder
+                .AddMeter(DiagnosticSettings.Meter.Value.Name)
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()
+                );
 
         return services;
     }
